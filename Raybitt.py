47a1640f98087f640c9f858ef2748ecef526c18a
MotherFnChris <76895814+MotherFnChris@users.noreply.github.com>
2023-07-16 11:06:25 -0400

    Add files via upload

--- Raybitt.py
+++ Raybitt.py
@@ -8,9 +8,12 @@ import sqlite3
 import configparser
 import os
 from nextcord.ext import commands
-import tracemalloc
-import random
-tracemalloc.start()
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+database_file = config['Database']['File']
+token = config['Bot']['Token']
 
 intents = discord.Intents.default()
 intents.typing = False
@@ -21,33 +24,17 @@ intents.guilds = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-database_file = config['Database']['File']
-token = config['Bot']['Token']
-
 
 def loadCog(extension):
     try:
         bot.load_extension(extension)
         print(f"Cog {extension} loaded.")
-    except commands.ExtensionNotFound:
-        print(f"Cog '{extension}' not found.")
-    except commands.ExtensionAlreadyLoaded:
-        print(f"Cog '{extension}' is already loaded.")
     except commands.ExtensionFailed as e:
         print(f"Cog '{extension}' failed to load. Reason: {e}")
 
 
 @bot.event
 async def on_ready():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-
-    database_file = config['Database']['File']
-    token = config['Bot']['Token']
-
     conn = sqlite3.connect(database_file)
     cursor = conn.cursor()
     cursor.execute('CREATE TABLE IF NOT EXISTS word_counts (id INTEGER PRIMARY KEY, word TEXT, count INTEGER)')
@@ -59,4 +46,10 @@ async def on_ready():
     print('------')
 
 
-bot.run(token)
+if __name__ == "__main__":
+    try:
+        bot.run(token)
+    except discord.LoginFailure:
+        print("Invalid token. Please check your bot token in config.ini.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
